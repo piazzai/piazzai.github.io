c9c01803de6f349ea0b017c5f68cd74bb54d1bc8
piazzai <42124135+piazzai@users.noreply.github.com>
2024-06-02 21:15:08 +0200

    Wipe website

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener(
-  "DOMContentLoaded",
-  (event) => {
-    particlesJS.load("particles-js", "/assets/json/particles.json", function () {
-      console.log("particles.js loaded - callback");
-    });
-  },
-  false
-);
